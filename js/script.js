84477da4ed0af972389ba393627d867cbd4d58de
Nguyen Luong Duy <33522847+ngylduy@users.noreply.github.com>
2021-09-24 11:25:29 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,7 +26,8 @@ function loadMusic(indexNumb){
   musicArtist.innerText = allMusic[indexNumb - 1].artist;
   musicImg.alt = allMusic[indexNumb - 1].artist;
   musicImg.title = allMusic[indexNumb - 1].artist;
-  musicImg.src = `https://cdn.jsdelivr.net/gh/ngylduy/storage/image/${allMusic[indexNumb - 1].src}.jpg`;
+  var getdatasrc = `https://cdn.jsdelivr.net/gh/ngylduy/storage/image/${allMusic[indexNumb - 1].src}.jpg`;
+  musicImg.attr('data-src',getdatasrc);
   mainAudio.src = `https://ia801400.us.archive.org/27/items/music_20210917/${allMusic[indexNumb - 1].src}.mp3`;
 }
 //play music function
